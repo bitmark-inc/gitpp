a590e2b935605fce39415293478e53bc2bedfd2a
Christopher Hall <hsw@bitmark.com>
2020-09-17 14:47:10 +0800

    [ocap-git-push-demo] select a capability feature
    create separate capabilities: e.g.
    
      cd /path/to/gitpp/ocap-git-push-demo
      make create-capbility CAPABILITY_NAME=myrepo GIT_URL=/path/to/myrepo BRANCH_RE='^refs/heads/(foo|bar)$'
      cd /path/to/my/repo
      sh /path/to/gitpp/ocap-git-push-demo/install-hooks.sh
      …comits…
      git push
    
    now capabilities are listed for selection.
    
    Signed-off-by: Christopher Hall <hsw@bitmark.com>

--- ocap-git-push-demo/install-hooks.sh
+++ ocap-git-push-demo/install-hooks.sh
@@ -5,6 +5,7 @@ src_dir="$(dirname "${0}")"
 dst_dir="${PWD}"
 
 hook_dir="${src_dir}/git-hooks"
+caps_dir="${src_dir}/ocaps"
 
 USAGE() {
   if [ -n "${1}" ]
@@ -20,15 +21,25 @@ USAGE() {
 # main program
 [ -d "${dst_dir}/git-hooks" ] && USAGE 'run in the destination repository not: %s' "${dst_dir}"
 [ -d "${hook_dir}" ] || USAGE 'cannot locate hook source dir: %' "${hook_dir}"
+[ -d "${caps_dir}" ] || USAGE 'cannot locate capability dir: %' "${caps_dir}"
 
 hook_dst="${dst_dir}/.git/hooks"
 [ -d "${hook_dst}" ] || USAGE 'the destination repository: %s  is not a git repository' "${dst_dir}"
+
+capability_list=$(ls -1 "${caps_dir}"/*.cap | \
+  while read file
+  do
+    [ ! -f "${file}" ] && continue
+    [ X"${file}" = X"${file%.cap}" ] && continue
+    file="cap:$(basename "${file}")"
+    printf '%s ' "${file}"
+  done)
 
 for s in "${hook_dir}"/*
 do
   bn="$(basename "${s}")"
   d="${hook_dst}/${bn}"
   printf 'installing: %s to: %s\n' "${s}" "${d}"
-  sed "s,@@JS_DIR@@,${src_dir},g" "${s}" > "${d}"
+  sed "s,@@JS_DIR@@,${src_dir},g;s,@@CAPABILITY@@,${capability_list},g" "${s}" > "${d}"
   chmod 755 "${d}"
 done
